53e5a4721d4f7cee86069cb094b2b792993f1086
Ed Mangimelli <edmangimelli@gmail.com>
2018-12-02 00:10:43 -0600

    day 2

--- 02/part1/main.rb
+++ 02/part1/main.rb
@@ -1,4 +1,24 @@
-input=[];File.readlines('input.txt').each{|l|break if l=="\n";input<<Integer(l)}
+input=[]; File.readlines('input.txt').each{|l|break if l=="\n";input << l.strip}
 
+twos = 0
+threes = 0
 
+letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
+input.each do |word|
+  word = word.split('')
+  has_two = false
+  has_three = false
+  letters.each do |letter|
+    filtered = word.select {|w| w == letter}
+    case filtered.length
+    when 3; has_three = true
+    when 2; has_two = true
+    end
+    break if has_three && has_two
+  end
+  threes += 1 if has_three
+  twos += 1 if has_two
+end
+
+puts threes * twos
--- 02/part2/main.rb
+++ 02/part2/main.rb
@@ -1,4 +1,32 @@
-input=[];File.readlines('input.txt').each{|l|break if l=="\n";input<<Integer(l)}
+input=[]; File.readlines('input.txt').each{|l|break if l=="\n";input << l.strip}
 
+def answer(a, b)
+  conflicts = []
+  i = 0
+  len = a.length
+  while i < len
+    conflicts << i if a[i] != b[i]
+    return nil if conflicts.length == 2
+    i += 1
+  end
+  i = conflicts[0]
+  return a[0...i] + a[i+1...len]
+end
+
+len = input.length
+i = 0
+while i < len
+  j = i + 1
+  while j < len
+    a = input[i]
+    b = input[j]
+    if ans = answer(a, b)
+      puts ans
+      exit
+    end
+    j += 1
+  end
+  i += 1
+end
 
 
